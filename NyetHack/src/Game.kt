95f1c9871e87a6e6adef3c4392042f033df18b15
NeimadTL <damientalbot26@gmail.com>
2025-02-25 14:23:06 -0400

    Add chapter 4: functions
    This chapter shows how to define standard functions and
    single-expression functions. It also show cases named function
    arguments and default arguments.

--- NyetHack/src/Game.kt
+++ NyetHack/src/Game.kt
@@ -3,11 +3,7 @@ fun main() {
     var healthPoints = 89
     val isBlessed = true
     val isImmortal = false
-
-    val aurVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (aurVisible) "GREEN" else "NONE"
-    //println(auraColor)
-    println("(Aura: $auraColor) (Blessed: ${if (isBlessed) "YES" else "NO"})")
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
 
     /*
     val healthStatus = if (healthPoints == 100) {
@@ -28,6 +24,25 @@ fun main() {
     println("$name $healthStatus")
     */
 
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+    //castFireball(5)
+    castFireball()
+}
+
+private fun printPlayerStatus(auraColor: String, isBlessed: Boolean, name: String, healthStatus: String) {
+    println("(Aura: $auraColor) (Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
+    val aurVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = if (aurVisible) "GREEN" else "NONE"
+    return auraColor
+}
+
+/* standard function
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
     val healthStatus = when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
@@ -39,5 +54,24 @@ fun main() {
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
     }
-    println("$name $healthStatus")
+    return healthStatus
+}
+*/
+
+// single-expression function
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
+     when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds but is healing quite quickly!"
+        } else {
+            "has some minor wounds"
+        }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
+
+private fun castFireball(numFireballs: Int = 2){
+    println("A glass of Fireball springs into existence. (x$numFireballs)")
 }
